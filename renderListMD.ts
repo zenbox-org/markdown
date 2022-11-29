eecb098686dcc84f773f75c21b922afa4e34d698
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-11-29 15:27:22 +0700

    CoinDoc

--- renderListMD.ts
+++ renderListMD.ts
@@ -5,6 +5,10 @@ export function renderListMD<T>(array: T[], renderer: Mapper<T, string> = identi
   return array.map(el => `* ${renderer(el)}`).join('\n')
 }
 
+export function renderListWithLabelsMD<T>(array: [string, T][], renderer: Mapper<T, string> = identity) {
+  return array.map(([label, el]) => `* ${label}: ${renderer(el)}`).join('\n')
+}
+
 export function renderLineMD<T>(array: T[], renderer: Mapper<T, string> = identity) {
   return array.map(renderer).join(', ')
 }
